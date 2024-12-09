c8228fc17213bbe842f0040e2355dd2f898dbd4b
jamjamstone <junghyun4545@naver.com>
2024-12-09 11:57:54 +0900

    오전내용 추가
    오전에 배운 내용을 추가함

--- Program.cs
+++ Program.cs
@@ -41,12 +41,19 @@
         {
 
             //변수
+            //string _carName;
             public int _posX; //멤버 변수 또는 필드 또는 구성요소
             public int _posY;// public 접근지정자
             public float _maxSpeed;
             private int _carNumber; //private는 안쓰면 자동으로 사용됨
-
+            // 외부에 공개할 필요 없는 변수를 private로 숨기고 
+            //외부에서 직접 접근하지 못하도록 숨기고 메소드를 통해서만 조작하게 하는걸
+            //정보 은닉이라고 한다. 
+            ///객체 내부데이터 변경을 실수로부터 지킬 수 있음
+            ///_posX 변수는 차의 위치일수도 있지만 주차장의 위치 콘솔창의 넓이인지 타인은
+            ///알 수 없기 때문에 그러한 실수를 줄이기 위해서 
             //기능, 메소드mothod
+            /*
             public void MoveForward(int posX)//함수내에서의 private도 메소드를 이용해서 접근가능
             {
                 this._posX += posX;//this. 은 이 메소드 바깥의 클래스 내부의 변수를
@@ -54,8 +61,59 @@
             }
             public int GetCarNumber()//객체가 반환도 할 수 있다.
             {
-                return _carNumber;
+                return _carNumber;// 외부에서 념겨준 인자값으로 세팅할 예정
+
+
             }
+
+            public void SetCarNum(int carNumber)// 0보다 작은 값이 들어오면 0으로 리셋 아니면 값 변화
+            {
+                if (carNumber < 0)
+                {
+                    _carNumber = 0;
+                }
+                else
+                {
+                    _carNumber = carNumber;
+                }
+            }*/
+
+            /// 프로퍼티 -> 속성 
+            /// 프로퍼티는 외부에 노출될 필요가 없는 맴버변수들을 정보은닉시켜준다
+            /// 또한 외부와 소통할 일이 있을 경우 직접 맴버변수와 소통하는 것이 아니라
+            /// 필터의 역활로도 기능한다.
+            public int PosX
+            {
+                //get;set;//접근자
+                get//get을 지우면 보는것이 불가능해짐
+                {
+                    return _posX;
+                }
+                set// set을 지우면 보는것만 가능
+                {
+                    if (value > 1920)
+                    {
+                        _posX = 1920;
+                    }
+                    else
+                    {
+                        _posX = value;// value에는 대입하는 값을 받는다.
+
+                    }
+                }
+            }
+            ///자동구현 프로퍼티
+            ///클래스에 변수가 없어도 자동으로 생성해줌 <summary>
+            ///제약이 필요하면 자동구현으로 하면 안됨
+            ///제약이 필요 없으면 자동구현 프로퍼티 사용가능
+            /// </summary>
+            public string MyName
+            {
+                get;set;
+            }
+
+
+
         }
         static void MoveForward(Car car, int toMove)
         {
@@ -64,17 +122,28 @@
         static void Main(string[] args)
         {
             Car car = new Car();//참조형 Car car;만 있을 경우 주소를 담을
+            car.PosX = 10;
+            Console.WriteLine(car.PosX);
+            car.MyName = "Bugahti";
+            Console.WriteLine(car.MyName);
+                
+
+
+
             //그릇만 담을 경우
             //int myNum = 4;//값
             //int[] intArray = new int[10];//참조
-            car._maxSpeed = 1.0f;
-            car._posX = 14;
+            //car._maxSpeed = 1.0f;
+            //car._posX = 14;
             //가비지 콜렉터가 힙에서 안쓰는 메모리를 회수해서 감
             //가비지 콜렉터가 많이 일할 수록랙이 걸리기 때문에 이를 막는 여러 기법이 존재
             //car = new Car();
             //Car car2 = car;//참조형은 주소를 복사한다.
-            car.MoveForward(10);// 자동차와 관련된 함수를 사용
-                
+            //car.MoveForward(10);// 자동차와 관련된 함수를 사용
+            //car.SetCarNum(-10);
+            /// 음수가 들어가도 작동함 이런 이유로 다른 개발자가
+            //사용할 때 실수할 수 있음
+            //Console.WriteLine(car.GetCarNumber());
 
 
 
